25fbf88295bffbd301f454e582256f2c0bf90b9e
Manaf A <179787304+MMA38@users.noreply.github.com>
2025-02-07 00:07:43 -0500

    Update project

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,220 @@
 import 'package:flutter/material.dart';
+import 'package:encrypted_shared_preferences/encrypted_shared_preferences.dart';
+//import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatelessWidget
+{   const MyApp({super.key});
 
+@override
+Widget build(BuildContext context)
+{
+  return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Flutter Demo',
+    theme: ThemeData(
+
+
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      useMaterial3: true,
+    ),
+    home: const MyHomePage(title: 'Week 4'),
+  );
+}
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: LoginPage(),
-      theme: ThemeData(
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+class _MyHomePageState extends State<MyHomePage> {
 
-    );
+  EncryptedSharedPreferences encryptedSharedPreferences = EncryptedSharedPreferences();
+  late TextEditingController _controllerLogin;
+  late TextEditingController _controllerPassword;
+  var imageSource = "images/question.png";
+
+  @override
+  void initState() {
+    super.initState();
+    _controllerLogin = TextEditingController();
+    _controllerPassword = TextEditingController();
+    //_controller.text = "The new text"; //initialize the texfield
+    loadEncryptedPrefs();
   }
-}
-class LoginPage extends StatefulWidget {
+
   @override
-  _LoginPageState createState() => _LoginPageState();
-}
+  void dispose() {
+    _controllerLogin.dispose();
+    _controllerPassword.dispose();
+    super.dispose();
+  }
 
-class _LoginPageState extends State<LoginPage> {
-  final TextEditingController _passwordController = TextEditingController();
-  String imageSource = 'images/question.png'; // Initial Image
-
-  void _checkPassword() {
-    setState(() {
-      if (_passwordController.text == 'QWERTY123') {
-        imageSource = 'images/idea.png'; // Light Bulb Image
+  void saveEncryptedPrefs()  {
+    encryptedSharedPreferences.setString('LoginName', _controllerLogin.text).then((bool success1) {
+      if (success1) {
+        encryptedSharedPreferences.setString('password', _controllerPassword.text).then((bool success2) {
+          if (success2) {
+            snackBar('User Name and password saved successfully');
+          } else {
+            snackBar('Failed to save');
+          }
+        });
       } else {
-        imageSource = 'images/stop.png'; // Stop Sign Image
+        snackBar('Failed to save');
       }
     });
   }
 
+  void loadEncryptedPrefs() {
+    //retrieve username
+    encryptedSharedPreferences.getString('LoginName').then((loginName) {
+      _controllerLogin.text = loginName;
+      //if username retrieved then retrieve pswd
+      encryptedSharedPreferences.getString('password').then((password) {
+        _controllerPassword.text = password;
+        //if username and password retrieved print a succes message
+        if (loginName!='' && password != '' )
+          snackBar('Username and Password have been Loaded');
+      });
+    });
+  }
 
+  void ClearEncryptedPrefs() {
+    encryptedSharedPreferences.clear().then((bool success) {
+      if (success) {
+        snackBar('All saved Usernames and Password have been removed');
+      } else {
+        snackBar('Usernames and Password have not been removed');
+      }
+    });
+  }
 
+  void ResetFields(){
+    _controllerLogin.text ='';
+    _controllerPassword.text = '';
+  }
+
+  void snackBar(String message){
+    var snackBar = SnackBar(
+      content: Text(message),
+      action: SnackBarAction(
+        label: 'Undo',  // Button label
+        onPressed: () {
+          ResetFields();
+          ScaffoldMessenger.of(context).hideCurrentSnackBar();
+        },
+      ),
+      duration: Duration(seconds: 5),
+      backgroundColor: Colors.green,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+  }
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
+    return Scaffold( //modifi
       appBar: AppBar(
 
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
 
-        title: Text("Flutter Demo Home Page")),
-
+        title: Text(widget.title,
+          style: TextStyle(
+            fontSize: 24, // Change size
+            fontWeight: FontWeight.bold, // Make bold
+            color: Colors.black87, // Change color
+          ),
+        ),
+      ),
       body: Center(
 
         child: Column(
 
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-             TextField(
-                decoration: InputDecoration(labelText: 'Login'),
+
+            TextField(controller: _controllerLogin,
+                decoration: InputDecoration(
+                    hintText:"",
+                    border: OutlineInputBorder(),
+                    labelText: "Log in"
+                )
             ),
-              TextField(
-                 controller: _passwordController,
-                 obscureText: true,
-                 decoration: InputDecoration(labelText: 'Password'),
+
+            TextField(controller: _controllerPassword,
+                obscureText:true,
+                decoration: InputDecoration(
+                  hintText:"",
+                  border: OutlineInputBorder(),
+                  labelText: "Password",
+
+                )
             ),
 
             ElevatedButton(
-              onPressed: _checkPassword,
-              child: Text('Login'),
-            ),
+              onPressed: () {
+                String password = _controllerPassword.value.text;
+                setState(() {
+                  if(password=="QWERTY123")
+                    imageSource = "images/idea.png";
+                  else
+                    imageSource = "images/stop.png";
 
-            Image.asset(
-              imageSource,
-              width: 250,
-              height: 250,
+                });
+
+                showDialog<String>(
+                  context: context,
+                  builder: (BuildContext context) => AlertDialog(
+                    title: const Text('Save information ! '),
+                    content: const Text('Would like to save username and password?'),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          saveEncryptedPrefs();
+                          Navigator.pop(context);
+                        },
+                        child: Text('Yes', style: TextStyle(fontSize: 30, color: Colors.blueAccent)),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          ClearEncryptedPrefs();
+                          Navigator.pop(context);
+                        },
+                        child: Text('No', style: TextStyle(fontSize: 30, color: Colors.blueAccent)),
+                      ),
+                    ],//action
+                  ),
+                );
+              },
+              child: Text('Login',
+                  style: TextStyle(fontSize: 30, color: Colors.blueAccent)),
             ),
+/*
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: ElevatedButton(
+                onPressed: () => ResetFields(),
+                child: Text ('Reset Fields',
+                  //style: TextStyle(fontSize: 30, color: Colors.blueAccent),
+                ),
+              ), //
+            ),*/
+            Image.asset(imageSource, width: 300, height: 300),
+
           ],
         ),
       ),
+
     );
   }
 }
