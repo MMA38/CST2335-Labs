bc2f10b3cc109ae7480473514b721fc4fbc1e5c4
Manaf A <179787304+MMA38@users.noreply.github.com>
2025-02-17 21:19:32 -0500

    Week 6 completed

--- lib/main.dart
+++ lib/main.dart
@@ -7,78 +7,113 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: LoginPage(),
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
-class LoginPage extends StatefulWidget {
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
   @override
-  _LoginPageState createState() => _LoginPageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
-  final TextEditingController _passwordController = TextEditingController();
-  String imageSource = 'images/question.png'; // Initial Image
+class _MyHomePageState extends State<MyHomePage> {
 
-  void _checkPassword() {
-    setState(() {
-      if (_passwordController.text == 'QWERTY123') {
-        imageSource = 'images/idea.png'; // Light Bulb Image
-      } else {
-        imageSource = 'images/stop.png'; // Stop Sign Image
-      }
-    });
-  }
-
-
-
+  var words = <String>[];
+  final TextEditingController _itemController = TextEditingController();
+  final TextEditingController _quantityController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
+        title: Text("Flutter Demo Home Page "),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text("Flutter Demo Home Page")),
-
-      body: Center(
-
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
 
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-             TextField(
-                decoration: InputDecoration(labelText: 'Login'),
-            ),
-              TextField(
-                 controller: _passwordController,
-                 obscureText: true,
-                 decoration: InputDecoration(labelText: 'Password'),
-            ),
+          children: [
 
-            ElevatedButton(
-              onPressed: _checkPassword,
-              child: Text('Login'),
-            ),
+            Row(children: [
+              Expanded(
+                child: TextField(
+                  controller: _itemController,
+                  decoration: InputDecoration(hintText: "type the item here",
+                    border: OutlineInputBorder(),),),
+              ),
+              Expanded(
+                child: TextField(
+                  controller: _quantityController,
+                  decoration: InputDecoration(hintText: "type the quantity here",
+                    border: OutlineInputBorder(),),
+                ),
+              ),
 
-            Image.asset(
-              imageSource,
-              width: 300,
-              height: 300,
-            ),
-          ],
+              ElevatedButton(onPressed: (){
+                if(_itemController.value.text.isNotEmpty && _quantityController.text.isNotEmpty) {
+                  setState(() {
+                    words.add("${_itemController.text} quantity: ${_quantityController.text}");
+                    _itemController.text = "";  // empty the field after adding item
+                    _quantityController.text = "";
+                  });
+                }else{
+                  const snackBar= SnackBar(content: Text("Please add item"));
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                };
+              }, child: Text("Click here"))
+            ],),
+
+            Expanded(
+                child: ListView.builder(
+                    itemCount: words.length,
+                    itemBuilder: (context, rowNum) {
+                      return GestureDetector(
+                        onLongPress: (){
+                          showDialog(context: context, builder: (context) {
+                            return AlertDialog(
+                              title: Text("Delete?"),
+                              content: Text ("Are you sure you want to delete the item?"),
+                              actions: [
+                                ElevatedButton(onPressed: (){
+                                  setState(() {
+                                    words.removeAt(rowNum);
+                                    Navigator.pop(context);
+                                  });
+                                },
+                                  child: Text("Yes"),),
+                                ElevatedButton(onPressed: (){
+                                  Navigator.pop(context);
+                                },
+                                  child: Text("No"),)
+                              ],
+                            );
+                          });
+                        },
+                        child: Row(
+                          mainAxisAlignment:MainAxisAlignment.center,
+                          children: [
+                            Text("${rowNum + 1}:"),
+                            Text(words[rowNum])
+                          ],),
+                      );
+                    }
+                )
+            )],
         ),
       ),
     );
